2c5d1401f2ed8b63c23888c3c9f55b7cbb5e4a22
drjbyrne <dr.j.byrne@gmail.com>
2013-07-24 09:28:24 +0100

    JB-part2 before cleanup

--- JB-part2.rb
+++ JB-part2.rb
@@ -38,6 +38,16 @@ end
 
 
 def rps_tournament_winner(tournament)
+      # Check if we're at a game
+      if tournament[0][0].is_a? String
+                return rps_game_winner(tournament)
+            end
+      # Otherwise keep going down the rabbit hole
+      return rps_game_winner([rps_tournament_winner(tournament[0]),rps_tournament_winner(tournament[1])])
+  end
+
+
+ def Rrps_tournament_winner(tournament)
 rounds = 0
   if tournament.length == 2 && tournament[1].length > 1
     rounds += 1
@@ -63,7 +73,7 @@ rounds = 0
         end
       end
     end
-  end
+end
 
   # puts "Rounds: ", rounds
 
@@ -71,7 +81,7 @@ results = Array.new
 #if rounds == 8
 
   if rounds == 7
-#  results[0][0][0][0][0][0] = rps_game_winner(tournament[0][0][0][0][0][0][0],tournament[0][0][0][0][0][0][1])
+    #  results[0][0][0][0][0][0] = rps_game_winner(tournament[0][0][0][0][0][0][0],tournament[0][0][0][0][0][0][1])
 
   elsif rounds == 6
 #  results[0][0][0][0][0]
@@ -137,10 +147,14 @@ end
 # return rps_game_winner([winner0H,winner1H])
 
 
-match = [["Armando", "P"], ["Dave", "S"]]
-rps_game_winner(match)
+tourney2 = [["Armando", "P"], ["Dave", "S"]]
 
-tourney = [
+tourney4 = [
+             [ ["Allen", "S"], ["Omer", "P"] ],
+             [ ["David E.", "R"], ["Richard X.", "P"] ]
+           ]
+
+tourney8 = [
     [
         [ ["Armando", "P"], ["Dave", "S"] ],
         [ ["Richard", "R"],  ["Michael", "S"] ],
@@ -151,10 +165,7 @@ tourney = [
     ]
 ]
 
-
-
-tourney16 =
-  [
+tourney16 =  [
        [
                 [
                              [ ["Armando", "P"], ["Dave", "S"] ],
@@ -177,7 +188,14 @@ tourney16 =
             ]
    ]
 
-tourney256 =
-[[[[[[[[["Axenus", "R"], ["Juste", "R"]], [["Oran", "R"], ["Cris", "S"]]], [[["Juan", "S"], ["Vincens", "P"]], [["Max", "S"], ["Mervin", "S"]]]], [[[["Ardell", "P"], ["Leo", "S"]], [["Ailesh", "S"], ["Anselmo", "P"]]], [[["Isadore", "R"], ["Debra", "S"]], [["Kyler", "P"], ["Anil", "P"]]]]], [[[[["William", "R"], ["Vencenzo", "P"]], [["Obie", "R"], ["Amado", "P"]]], [[["Rock", "S"], ["Dhaval", "S"]], [["Clay", "S"], ["Sherwood", "P"]]]], [[[["Audie", "R"], ["Debra", "R"]], [["Al", "S"], ["Lowell", "R"]]], [[["Abban", "S"], ["Cristiano", "R"]], [["Fermin", "P"], ["Steve", "P"]]]]]], [[[[[["Everette", "P"], ["Anand", "S"]], [["Chasiel", "S"], ["Gratian", "P"]]], [[["Ellis", "P"], ["Quentin", "R"]], [["Juston", "S"], ["Elza", "S"]]]], [[[["Elmo", "P"], ["Jamel", "R"]], [["Ryne", "P"], ["Arkhip", "S"]]], [[["Vernie", "P"], ["Jordon", "S"]], [["Gustav", "R"], ["Sidney", "R"]]]]], [[[[["Arjun", "P"], ["Esteban", "P"]], [["Brenton", "P"], ["Jaidev", "S"]]], [[["Houston", "P"], ["Devdas", "R"]], [["Dexter", "P"], ["Bala", "P"]]]], [[[["Darien", "S"], ["Vernados", "R"]], [["Trever", "P"], ["Mel", "S"]]], [[["Amato", "S"], ["Chandler", "R"]], [["Tod", "R"], ["Donald", "R"]]]]]]], [[[[[[["Sixto", "R"], ["Conall", "P"]], [["Andrea", "S"], ["Rusty", "P"]]], [[["Akemi", "S"], ["Serenus", "P"]], [["Gearld", "R"], ["Chadd", "S"]]]], [[[["Hagan", "R"], ["Kermit", "S"]], [["Uinseann", "S"], ["Hunter", "R"]]], [[["Leonel", "S"], ["Resheph", "S"]], [["Reyes", "R"], ["Maury", "P"]]]]], [[[[["Garett", "S"], ["Owen", "S"]], [["Kadeem", "R"], ["Graziano", "R"]]], [[["Lochan", "R"], ["Marinos", "P"]], [["Cory", "S"], ["Cianan", "R"]]]], [[[["Kavindra", "P"], ["Shane", "P"]], [["Kyle", "R"], ["Moises", "S"]]], [[["Marinos", "S"], ["Clovis", "P"]], [["Anluan", "S"], ["Cezar", "P"]]]]]], [[[[[["Jarvis", "S"], ["Dhaval", "R"]], [["Rickey", "R"], ["Christin", "P"]]], [[["Benigno", "P"], ["Gustavo", "R"]], [["Allyn", "P"], ["Ailesh", "S"]]]], [[[["Ossian", "P"], ["Arcadio", "S"]], [["Avram", "P"], ["Conan", "S"]]], [[["Jevon", "R"], ["Ermanno", "R"]], [["Felice", "P"], ["Margarit", "S"]]]]], [[[[["Baruch", "R"], ["Dewey", "P"]], [["Loyd", "R"], ["Dax", "P"]]], [[["Tad", "P"], ["Edison", "S"]], [["Deon", "S"], ["Asher", "R"]]]], [[[["Eochaidh", "S"], ["Solomon", "S"]], [["Abdullah", "S"], ["Ivory", "P"]]], [[["Eachann", "R"], ["Devon", "P"]], [["Reno", "R"], ["Marie", "R"]]]]]]]], [[[[[[[["Abdul", "S"], ["Pedro", "P"]], [["Tyrone", "P"], ["Peli", "R"]]], [[["Forrest", "P"], ["Prentice", "P"]], [["Ronan", "P"], ["Lukas", "S"]]]], [[[["Bryon", "S"], ["Codie", "S"]], [["Ciarrai", "P"], ["Thrasius", "R"]]], [[["Aldo", "R"], ["Tylor", "P"]], [["Kendrick", "R"], ["Derry", "P"]]]]], [[[[["Branduff", "S"], ["Tavor", "S"]], [["Lonnie", "P"], ["Doyle", "S"]]], [[["Kumakichi", "P"], ["Jesus", "P"]], [["Nikolas", "S"], ["Isha", "R"]]]], [[[["Darion", "P"], ["Patrick", "P"]], [["Emory", "S"], ["Wylie", "R"]]], [[["Broderic", "S"], ["Allyn", "P"]], [["Isidoros", "R"], ["Rama", "R"]]]]]], [[[[[["Enrique", "P"], ["Euston", "P"]], [["Attis", "P"], ["Spiridon", "S"]]], [[["Avaro", "S"], ["Arthur", "R"]], [["Micah", "R"], ["Eusebio", "R"]]]], [[[["Abel", "P"], ["Innokenti", "S"]], [["Gustaf", "P"], ["Sonny", "S"]]], [[["Demetri", "S"], ["Achilles", "P"]], [["Agust", "S"], ["Antwon", "R"]]]]], [[[[["Enoch", "S"], ["Tyrel", "R"]], [["Sixto", "S"], ["Beolagh", "S"]]], [[["Kristoph", "P"], ["Kavindra", "S"]], [["Julius", "P"], ["Andrea", "P"]]]], [[[["Leif", "R"], ["Tyron", "P"]], [["Ardal", "S"], ["Furman", "P"]]], [[["Rogelio", "P"], ["Zakary", "R"]], [["Prokhop", "R"], ["Aries", "R"]]]]]]], [[[[[[["Homer", "R"], ["Laurence", "P"]], [["Carmelo", "P"], ["Merlin", "S"]]], [[["Prokhop", "S"], ["Amado", "S"]], [["Caesar", "R"], ["Davin", "S"]]]], [[[["Marion", "R"], ["King", "R"]], [["Colombain", "S"], ["Kurt", "P"]]], [[["Naresh", "P"], ["Fehin", "P"]], [["Garland", "P"], ["Rico", "R"]]]]], [[[[["Coby", "R"], ["Harley", "P"]], [["Asher", "S"], ["Mikhail", "P"]]], [[["Elmore", "R"], ["Bruno", "R"]], [["Fabrice", "P"], ["Maximus", "P"]]]], [[[["Venedictos", "R"], ["Crawford", "S"]], [["Domingo", "P"], ["Galen", "S"]]], [[["Ryan", "P"], ["Anlon", "R"]], [["Brandon", "R"], ["Jed", "S"]]]]]], [[[[[["Walter", "S"], ["Amato", "P"]], [["Aineias", "S"], ["Kamil", "S"]]], [[["Aharon", "S"], ["Jaylen", "R"]], [["Payton", "P"], ["Lynn", "P"]]]], [[[["Avanindra", "S"], ["Krishna", "R"]], [["Trevor", "S"], ["Adhideva", "R"]]], [[["Leongard", "P"], ["Dwane", "R"]], [["Dale", "R"], ["Hakeem", "S"]]]]], [[[[["Arnd", "R"], ["Balendin", "S"]], [["Abdiel", "P"], ["Houston", "P"]]], [[["Vlad", "R"], ["Anastasios", "R"]], [["Mason", "R"], ["Shawn", "R"]]]], [[[["Denzel", "R"], ["Rigobert", "R"]], [["Wayne", "R"], ["Demario", "S"]]], [[["Colon", "P"], ["Clyde", "S"]], [["Faron", "S"], ["Gaston", "S"]]]]]]]]]
+tourney128 = [
+              [[[[[[["Ea", "R"], ["Hunter", "S"]], [["Mohamed", "S"], ["Eric", "P"]]], [[["Ali", "P"], ["Leander", "P"]], [["Dominic", "R"], ["Samir", "P"]]]], [[[["Ocie", "S"], ["Epifanio", "R"]], [["Vasant", "S"], ["Anselmo", "R"]]], [[["Murphy", "S"], ["Angus", "P"]], [["Kim", "S"], ["Justyn", "P"]]]]], [[[[["Akshan", "R"], ["Uberto", "P"]], [["Ezell", "P"], ["Xavier", "S"]]], [[["Buddie", "R"], ["Rodrick", "P"]], [["Spenser", "S"], ["Joy", "R"]]]], [[[["Jorden", "S"], ["Xavier", "S"]], [["Tulio", "S"], ["Sudas", "P"]]], [[["Gail", "P"], ["Gyala", "P"]], [["Logan", "P"], ["Emanuel", "R"]]]]]], [[[[[["Balendin", "P"], ["Armando", "S"]], [["Innocenzio", "R"], ["Branden", "P"]]], [[["Gurudatta", "S"], ["Diego", "R"]], [["Todd", "P"], ["Antone", "P"]]]], [[[["Sal", "P"], ["Abban", "R"]], [["Cathal", "P"], ["Jesse", "R"]]], [[["Marshall", "R"], ["Yukio", "P"]], [["Vlad", "R"], ["Devonta", "P"]]]]], [[[[["Aeneas", "P"], ["Delbert", "S"]], [["Jorden", "R"], ["Leopoldo", "R"]]], [[["Tomi", "P"], ["Nicodemo", "S"]], [["Glen", "P"], ["Lothar", "S"]]]], [[[["Ferris", "P"], ["Brent", "R"]], [["Llewelyn", "R"], ["Derrell", "S"]]], [[["Omer", "P"], ["Soterios", "R"]], [["Caoimhin", "P"], ["Jere", "P"]]]]]]], [[[[[[["Sohan", "S"], ["Radames", "S"]], [["Cliff", "R"], ["Warden", "R"]]], [[["Jermaine", "R"], ["Meallan", "S"]], [["Majid", "R"], ["Emmitt", "P"]]]], [[[["Gill", "S"], ["descrition", "S"]], [["Joy", "R"], ["Aakesh", "P"]]], [[["Isai", "S"], ["Jesus", "R"]], [["Adrien", "R"], ["Karl", "P"]]]]], [[[[["Clarence", "S"], ["Glendon", "S"]], [["Camden", "R"], ["Nikhil", "P"]]], [[["Garrison", "S"], ["Achaius", "R"]], [["Uinseann", "S"], ["Christian", "P"]]]], [[[["Ralph", "R"], ["Canice", "S"]], [["Percival", "S"], ["Prentice", "P"]]], [[["Bennett", "S"], ["Aeolus", "R"]], [["Nadim", "R"], ["Blaine", "P"]]]]]], [[[[[["Alvis", "P"], ["Albin", "S"]], [["Esmond", "P"], ["Laddie", "P"]]], [[["Adalard", "S"], ["Charles", "S"]], [["Faust", "P"], ["Lavanaa", "P"]]]], [[[["Esmond", "P"], ["Aakil", "P"]], [["Killian", "R"], ["Darrick", "S"]]], [[["Grover", "R"], ["Khalil", "R"]], [["Euston", "P"], ["Brayden", "P"]]]]], [[[[["Rahsaan", "P"], ["Pablo", "R"]], [["Tito", "P"], ["Earl", "S"]]], [[["Richmond", "P"], ["Delbert", "R"]], [["Alonzo", "P"], ["Jay", "P"]]]], [[[["Dallas", "S"], ["Jaylin", "R"]], [["Humfried", "P"], ["Joey", "P"]]], [[["Lares", "P"], ["Howell", "R"]], [["Griffin", "S"], ["Aatmadeva", "P"]]]]]]]]
 
-puts rps_tournament_winner(tourney256)
+
+tourney256 = [
+              [[[[[[[["Axenus", "R"], ["Juste", "R"]], [["Oran", "R"], ["Cris", "S"]]], [[["Juan", "S"], ["Vincens", "P"]], [["Max", "S"], ["Mervin", "S"]]]], [[[["Ardell", "P"], ["Leo", "S"]], [["Ailesh", "S"], ["Anselmo", "P"]]], [[["Isadore", "R"], ["Debra", "S"]], [["Kyler", "P"], ["Anil", "P"]]]]], [[[[["William", "R"], ["Vencenzo", "P"]], [["Obie", "R"], ["Amado", "P"]]], [[["Rock", "S"], ["Dhaval", "S"]], [["Clay", "S"], ["Sherwood", "P"]]]], [[[["Audie", "R"], ["Debra", "R"]], [["Al", "S"], ["Lowell", "R"]]], [[["Abban", "S"], ["Cristiano", "R"]], [["Fermin", "P"], ["Steve", "P"]]]]]], [[[[[["Everette", "P"], ["Anand", "S"]], [["Chasiel", "S"], ["Gratian", "P"]]], [[["Ellis", "P"], ["Quentin", "R"]], [["Juston", "S"], ["Elza", "S"]]]], [[[["Elmo", "P"], ["Jamel", "R"]], [["Ryne", "P"], ["Arkhip", "S"]]], [[["Vernie", "P"], ["Jordon", "S"]], [["Gustav", "R"], ["Sidney", "R"]]]]], [[[[["Arjun", "P"], ["Esteban", "P"]], [["Brenton", "P"], ["Jaidev", "S"]]], [[["Houston", "P"], ["Devdas", "R"]], [["Dexter", "P"], ["Bala", "P"]]]], [[[["Darien", "S"], ["Vernados", "R"]], [["Trever", "P"], ["Mel", "S"]]], [[["Amato", "S"], ["Chandler", "R"]], [["Tod", "R"], ["Donald", "R"]]]]]]], [[[[[[["Sixto", "R"], ["Conall", "P"]], [["Andrea", "S"], ["Rusty", "P"]]], [[["Akemi", "S"], ["Serenus", "P"]], [["Gearld", "R"], ["Chadd", "S"]]]], [[[["Hagan", "R"], ["Kermit", "S"]], [["Uinseann", "S"], ["Hunter", "R"]]], [[["Leonel", "S"], ["Resheph", "S"]], [["Reyes", "R"], ["Maury", "P"]]]]], [[[[["Garett", "S"], ["Owen", "S"]], [["Kadeem", "R"], ["Graziano", "R"]]], [[["Lochan", "R"], ["Marinos", "P"]], [["Cory", "S"], ["Cianan", "R"]]]], [[[["Kavindra", "P"], ["Shane", "P"]], [["Kyle", "R"], ["Moises", "S"]]], [[["Marinos", "S"], ["Clovis", "P"]], [["Anluan", "S"], ["Cezar", "P"]]]]]], [[[[[["Jarvis", "S"], ["Dhaval", "R"]], [["Rickey", "R"], ["Christin", "P"]]], [[["Benigno", "P"], ["Gustavo", "R"]], [["Allyn", "P"], ["Ailesh", "S"]]]], [[[["Ossian", "P"], ["Arcadio", "S"]], [["Avram", "P"], ["Conan", "S"]]], [[["Jevon", "R"], ["Ermanno", "R"]], [["Felice", "P"], ["Margarit", "S"]]]]], [[[[["Baruch", "R"], ["Dewey", "P"]], [["Loyd", "R"], ["Dax", "P"]]], [[["Tad", "P"], ["Edison", "S"]], [["Deon", "S"], ["Asher", "R"]]]], [[[["Eochaidh", "S"], ["Solomon", "S"]], [["Abdullah", "S"], ["Ivory", "P"]]], [[["Eachann", "R"], ["Devon", "P"]], [["Reno", "R"], ["Marie", "R"]]]]]]]], [[[[[[[["Abdul", "S"], ["Pedro", "P"]], [["Tyrone", "P"], ["Peli", "R"]]], [[["Forrest", "P"], ["Prentice", "P"]], [["Ronan", "P"], ["Lukas", "S"]]]], [[[["Bryon", "S"], ["Codie", "S"]], [["Ciarrai", "P"], ["Thrasius", "R"]]], [[["Aldo", "R"], ["Tylor", "P"]], [["Kendrick", "R"], ["Derry", "P"]]]]], [[[[["Branduff", "S"], ["Tavor", "S"]], [["Lonnie", "P"], ["Doyle", "S"]]], [[["Kumakichi", "P"], ["Jesus", "P"]], [["Nikolas", "S"], ["Isha", "R"]]]], [[[["Darion", "P"], ["Patrick", "P"]], [["Emory", "S"], ["Wylie", "R"]]], [[["Broderic", "S"], ["Allyn", "P"]], [["Isidoros", "R"], ["Rama", "R"]]]]]], [[[[[["Enrique", "P"], ["Euston", "P"]], [["Attis", "P"], ["Spiridon", "S"]]], [[["Avaro", "S"], ["Arthur", "R"]], [["Micah", "R"], ["Eusebio", "R"]]]], [[[["Abel", "P"], ["Innokenti", "S"]], [["Gustaf", "P"], ["Sonny", "S"]]], [[["Demetri", "S"], ["Achilles", "P"]], [["Agust", "S"], ["Antwon", "R"]]]]], [[[[["Enoch", "S"], ["Tyrel", "R"]], [["Sixto", "S"], ["Beolagh", "S"]]], [[["Kristoph", "P"], ["Kavindra", "S"]], [["Julius", "P"], ["Andrea", "P"]]]], [[[["Leif", "R"], ["Tyron", "P"]], [["Ardal", "S"], ["Furman", "P"]]], [[["Rogelio", "P"], ["Zakary", "R"]], [["Prokhop", "R"], ["Aries", "R"]]]]]]], [[[[[[["Homer", "R"], ["Laurence", "P"]], [["Carmelo", "P"], ["Merlin", "S"]]], [[["Prokhop", "S"], ["Amado", "S"]], [["Caesar", "R"], ["Davin", "S"]]]], [[[["Marion", "R"], ["King", "R"]], [["Colombain", "S"], ["Kurt", "P"]]], [[["Naresh", "P"], ["Fehin", "P"]], [["Garland", "P"], ["Rico", "R"]]]]], [[[[["Coby", "R"], ["Harley", "P"]], [["Asher", "S"], ["Mikhail", "P"]]], [[["Elmore", "R"], ["Bruno", "R"]], [["Fabrice", "P"], ["Maximus", "P"]]]], [[[["Venedictos", "R"], ["Crawford", "S"]], [["Domingo", "P"], ["Galen", "S"]]], [[["Ryan", "P"], ["Anlon", "R"]], [["Brandon", "R"], ["Jed", "S"]]]]]], [[[[[["Walter", "S"], ["Amato", "P"]], [["Aineias", "S"], ["Kamil", "S"]]], [[["Aharon", "S"], ["Jaylen", "R"]], [["Payton", "P"], ["Lynn", "P"]]]], [[[["Avanindra", "S"], ["Krishna", "R"]], [["Trevor", "S"], ["Adhideva", "R"]]], [[["Leongard", "P"], ["Dwane", "R"]], [["Dale", "R"], ["Hakeem", "S"]]]]], [[[[["Arnd", "R"], ["Balendin", "S"]], [["Abdiel", "P"], ["Houston", "P"]]], [[["Vlad", "R"], ["Anastasios", "R"]], [["Mason", "R"], ["Shawn", "R"]]]], [[[["Denzel", "R"], ["Rigobert", "R"]], [["Wayne", "R"], ["Demario", "S"]]], [[["Colon", "P"], ["Clyde", "S"]], [["Faron", "S"], ["Gaston", "S"]]]]]]]]]
+
+
+rps_game_winner(tourney2)
+
+puts rps_tournament_winner(tourney4)
