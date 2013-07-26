e1b8b07eecc7da335749e1d26baec3c06a6fa7a1
drjbyrne <dr.j.byrne@gmail.com>
2013-07-26 22:44:11 +0100

    Update part6

--- JB-part6.rb
+++ JB-part6.rb
@@ -1,5 +1,5 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'dollar' => 1.000, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -8,25 +8,29 @@ class Numeric
       super
     end
   end
+  def in(curr)
+    singular_currency = curr.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+            self / @@currencies[singular_currency]
+          else
+            super
+          end
+    end
 end
-
-# Define the instance method Numeric#in
-
-puts 5.yen
-
-# 5.dollars.in(:euros)
-# 10.euros.in(:rupees)
 
 
 
 class String
-
-
+  def palindrome?
+    @testgs = self.downcase.gsub(/\W/,'')
+    @testgs == @testgs.reverse
+  end
 end
 
 
-
 module Enumerable
-
-
+  def palindrome?
+        @tgs = self
+        @tgs == @tgs.reverse
+      end
 end
