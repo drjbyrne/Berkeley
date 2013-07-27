264b94f4bc8d15561eaf810098a801fc3e15f36e
drjbyrne <dr.j.byrne@gmail.com>
2013-07-27 13:34:16 +0100

    update part6

--- JB-part6.rb
+++ JB-part6.rb
@@ -18,8 +18,6 @@ class Numeric
     end
 end
 
-
-
 class String
   def palindrome?
     @testgs = self.downcase.gsub(/\W/,'')
@@ -27,10 +25,21 @@ class String
   end
 end
 
-
 module Enumerable
   def palindrome?
-        @tgs = self
-        @tgs == @tgs.reverse
-      end
+  if self.is_a?(Hash)
+    puts "It's a Hash"
+    return
+    else
+      @tgs = self
+      @tgs == @tgs.reverse
+    end
+    end
 end
+
+
+puts "Madam I'm Adam".palindrome?
+puts [1,2,3,2,1].palindrome?
+puts [1,3,2,4,1].palindrome?
+{"x" => 3, "y" => 2}.palindrome?
+puts [[1,2],[2],[1,2,3],[2],[1,2]].palindrome?
